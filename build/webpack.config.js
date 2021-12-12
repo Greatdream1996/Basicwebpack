a705447ff5c276278d9f2b011d1bde65c1f7dc8f
liao <1742992503@qq.com>
2021-12-12 23:02:32 +0800

    webpackFile

---
+++ build/webpack.config.js
@@ -0,0 +1,114 @@
+const path = require('path')
+const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const devMode = process.argv.indexOf('--mode=production') === -1
+module.exports = {
+  entry: {
+    main: path.resolve(__dirname, '../src/index.js')
+  },
+  output: {
+    path: path.resolve(__dirname, '../dist'),
+    filename: 'js/[name].[hash:8].js',
+    chunkFilename: 'js/[name].[hash:8].js'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use:[{
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }],
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              plugins: [require('autoprefixer')]
+            }
+          }
+        ]
+      },
+      {
+        test: /\.less$/,
+        use: [
+          MiniCssExtractPlugin.loader,
+          'css-loader',
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                indent: 'postcss',
+                plugins: [require('autoprefixer')]
+              }
+            }
+          },
+          'less-loader'
+        ]
+      },
+      {
+        test: /\.(jep?g|png|gif)$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 10240,
+            fallback: {
+              loader: 'file-loader',
+              options: {
+                name: 'img/[name].[hash:8].[ext]'
+              }
+            }
+          }
+        }
+      },
+      {
+        test: /\.(mp4|webm|ogg|mp3|wav|flac|aac)(\?.*)?$/,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 10240,
+            fallback: {
+              loader: 'file-loader',
+              options: {
+                name: 'media/[name].[hash:8].[ext]'
+              }
+            }
+          }
+        }
+      },
+      {
+        test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/i,
+        use: {
+          loader: 'url-loader',
+          options: {
+            limit: 10240,
+            fallback: {
+              loader: 'file-loader',
+              options: {
+                name: 'media/[name].[hash:8].[ext]'
+              }
+            }
+          }
+        }
+      }
+    ]
+  },
+  plugins: [
+    new CleanWebpackPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, '../public/index.html')
+    }),
+    new MiniCssExtractPlugin({
+      filename: devMode ? '[name].css' : '[name].[hash].css',
+      chunkFilename: devMode ? '[id].css' : '[id].[hash].css'
+    })
+  ]
+}
